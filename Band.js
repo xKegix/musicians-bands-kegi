ea5fe3cdb6550be3735c28b720da9e8b1b4fb211
keginold philip <xkegix@users.noreply.github.com>
2023-02-17 17:44:46 +0000

    added show count, delete / update

--- Band.js
+++ Band.js
@@ -3,7 +3,8 @@ const {Sequelize, sequelize} = require('./db');
 // TODO - define the Band model
 let Band = sequelize.define('band', {
     name: Sequelize.STRING,
-    genre: Sequelize.STRING
+    genre: Sequelize.STRING,
+    showCount: Sequelize.INTEGER
 })
 
 module.exports = {
--- index.test.js
+++ index.test.js
@@ -12,14 +12,66 @@ describe('Band and Musician Models', () => {
         await sequelize.sync({ force: true });
     })
 
+
+    // Band create
     test('can create a Band', async () => {
         // TODO - test creating a band
-        const testBand = await Band.create({name: 'TestBand', genre: 'Rap'});
+        const testBand = await Band.create({name: 'TestBand', genre: 'Rap', showCount: 13});
 
-        expect(testBand.name).toBe('TestBand');
-        expect(testBand.genre).toBe('Rap');
-    
+        const createdBand = await Band.findByPk(testBand.id);
+
+        expect(createdBand.name).toBe('TestBand');
+        expect(createdBand.genre).toBe('Rap');
+        expect(createdBand.showCount).toBe(13);
     })
+
+    // Band update
+
+    test('can update a Band', async () => {
+        const testBand = await Band.create({name: 'Band1', genre: 'Rock', showCount: 178});
+
+        await testBand.update({ name: 'Band1-Updated', showCount: 199});
+
+        const updatedBand = await Band.findByPk(testBand.id);
+
+        expect(updatedBand.name).toBe('Band1-Updated');
+        expect(updatedBand.genre).toBe('Rock');
+        expect(updatedBand.showCount).toBe(199);
+    })
+
+
+    // Band delete
+    test('can delete a Band', async () => {
+        const testBand = await Band.create({name: 'Band2', genre: 'RnB', showCount: 42});
+
+        let deleteBand = await Band.findByPk(testBand.id);
+        expect(deleteBand).not.toBeNull();
+
+        await Band.destroy({ where: { id: testBand.id }});
+
+        deleteBand = await Band.findByPk(testBand.id);
+        expect(deleteBand).toBeNull();
+    })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     test('can create a Musician', async () => {
         // TODO - test creating a musician
